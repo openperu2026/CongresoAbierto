7f528d3c7e564b44be50a1c8ca98104586f1018f
Cesar Nunez <ca.nunezhuaman@gmail.com>
2026-01-17 16:02:17 -0600

    feat(crud-bills): Created crud operations for RawBillDocuments

--- backend/database/crud/bills.py
+++ backend/database/crud/bills.py
@@ -1,6 +1,6 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from backend.database.raw_models import RawBill
+from backend.database.raw_models import RawBill, RawBillDocument
 
 ########################################
 # Raw Bill CRUD Operations
@@ -56,6 +56,62 @@ def mark_raw_bill_processed(session: Session, bill_id: str) -> bool:
         session.query(RawBill)
         .filter(RawBill.id == bill_id, RawBill.timestamp == max_ts)
         .update({RawBill.processed: True}, synchronize_session=False)
+    )
+    session.commit()
+    return updated == 1
+
+########################################
+# RawBillDocuments CRUD Operations
+########################################
+
+def get_documents_by_id(session: Session, bill_id: str, seguimiento_id: str) -> list[RawBillDocument]:
+    """
+    Gets all the RawBillDocuments from the DB by querying for bill and step
+
+    Args:
+        session (Session): Raw DB Session from Open Peru DB
+        bill_id (str): unique identifier for the bill
+        seguimiento_id (str): unique identifier for the step
+
+    Returns:
+        list[RawBillDocument]: list of RawBillDocuments associated with the bill and step
+    """
+    return session.query(RawBillDocument).filter(
+        RawBillDocument.bill_id == bill_id, 
+        RawBillDocument.seguimiento_id == seguimiento_id,
+        RawBillDocument.last_update == True).all()
+
+def mark_raw_bill_document_processed(session: Session, bill_id: str, seguimiento_id: str, archivo_id: str) -> bool:
+    """
+    Utility funtion to update the processed attribute in the RawDB
+
+    Args:
+        - session (Session): Raw DB Session from Open Peru DB
+        - bill_id (str): unique identifier from the bill
+        - seguimiento_id (str): unique identifier for the step
+        - archivo_id (str): unique identifier for the file
+    """
+    max_ts = (
+        session.query(func.max(RawBillDocument.timestamp))
+        .filter(
+            RawBillDocument.bill_id == bill_id,
+            RawBillDocument.seguimiento_id == seguimiento_id,
+            RawBillDocument.archivo_id == archivo_id
+            )
+        .scalar()
+    )
+    if max_ts is None:
+        return False
+
+    updated = (
+        session.query(RawBillDocument)
+        .filter(
+            RawBillDocument.bill_id == bill_id,
+            RawBillDocument.seguimiento_id == seguimiento_id,
+            RawBillDocument.archivo_id == archivo_id,        
+            RawBillDocument.timestamp == max_ts
+            )
+        .update({RawBillDocument.processed: True}, synchronize_session=False)
     )
     session.commit()
     return updated == 1
